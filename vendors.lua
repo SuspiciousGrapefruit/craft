3a5fdb7cce0250a4e6bd2e05f30c767dd3f57553
SuspiciousGrapefruit <suspiciousgrapefruit@gmail.com>
2017-11-23 04:27:36 -0700

    Updated recipes list

--- vendors.lua
+++ vendors.lua
@@ -1,5 +1,7 @@
 return {
     {name = "Graine", zone = 249},
     {name = "Deadly Minnow",  zone = 244},
-    {name = "Preterig", zone=256},
+    {name = "Preterig", zone = 256},
+    {name = "Brunhilde", zone = 235},
 }
+
